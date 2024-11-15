6e69d92d66a47772d91042f382f6e2813b64d4e2
Dmitriy Strelnik <dimonstrel@gmail.com>
2024-11-15 21:13:50 +0300

    11th Homework

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -15,112 +15,110 @@ sap.ui.define([
 		formatter: formatter,
 
         onInit: function () {
-            const oViewModel = new JSONModel({
-                sTotal: 0,
-                sITBKey: 'All',
-                selection: {}
-            });
-            this.setModel(oViewModel, "worklistView");
-            this.getOwnerComponent().getModel().setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+			var oViewModel = new JSONModel({
+				sTotal: '0',
+			});
+    		this.setModel(oViewModel, "worklistView");
+			this.getOwnerComponent().getModel().setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+   			 this._loadData();
         },
+		_loadData: function() {
+			this.getOwnerComponent().getModel().read("/zjblessons_base_Headers", {
+				success: function(oData) {
+					oData.results.forEach(function(item, index) {
+						item.order = index+1;
+					});
+					this.getModel("worklistView").setData({ items: oData.results });
+					this._getTotalItems();
+				}.bind(this),
+				error: function(oError) {
+					sap.m.MessageToast.show("Ошибка загрузки данных.");
+				}
+			});
+		},
         onBeforeRendering: function () {
             this._bindTable();
         },
         _bindTable: function (bIsRefresh = false) {
-            const oTable = this.getView().byId('table');
+			const oTable = this.getView().byId('table');
+			oTable.bindItems({
+				path: 'worklistView>/items',
+				templateShareable: false,
+				template: this._getTableTemplate(),
+				events: {
+					dataReceived: () => {
+						if (bIsRefresh) {
+							sap.m.MessageToast.show("Таблица успешно обновлена");
+						}
+					}
+				},
+			});
+		},
+		onDrop: function(oEvent) {
+            var oDragged = oEvent.getParameter("draggedControl");
+            var oDropped = oEvent.getParameter("droppedControl");
+            var sInsertPosition = oEvent.getParameter("dropPosition");
 
-            const oGroupSorter = new Sorter('RegionText', true, true);
+            var oTable = this.byId("table");
+            var aItems = this.getModel("worklistView").getProperty("/items");
 
-            oTable.bindItems({
-                path: '/zjblessons_base_Headers',
-                sorter: oGroupSorter,
-                templateShareable: false,
-                template: this._getTableTemplate(),
-                filters: this._getTableFilters(),
-                urlParameters: {
-                    $select: 'HeaderID,DocumentNumber,DocumentDate,PlantText,RegionText,Description,Created,Version'
-                },
-                events: {
-                    dataReceived: () => {
-                        this._getTotalItems();
-                        if (bIsRefresh) {
-                            sap.m.MessageToast.show("Table successfully updated");
-                        }
-                    }
-                },
-            });
+            var iDraggedIndex = oTable.indexOfItem(oDragged);
+            var iDroppedIndex = oTable.indexOfItem(oDropped);
+
+            if (sInsertPosition === "After") {
+                iDroppedIndex++;
+            }
+
+            var aMovedItem = aItems.splice(iDraggedIndex, 1);
+            aItems.splice(iDroppedIndex, 0, aMovedItem[0]);
+
+            this.getModel("worklistView").setProperty("/items", aItems);
         },
+				
 
         _getTableTemplate: function () {
             const oTemplate = new sap.m.ColumnListItem({
-                highlight: "{= ${Version} ===  'A' ? 'Success' : 'Error'}",
+                highlight: "{= ${worklistView>Version} ===  'D' ? 'Error' : 'Success'}",
                 type: 'Inactive', 
                 cells: [
+					new sap.m.Text({
+                        text: "{worklistView>order}"
+                    }),
                     new sap.m.Text({
-                        text: "{DocumentNumber}"
+                        text: "{worklistView>DocumentNumber}"
                     }),
                     new sap.m.Text({
                         text: {
-                            path: "DocumentDate",
+                            path: "worklistView>DocumentDate",
                             type: "sap.ui.model.type.Date",
                             formatOptions: { pattern: "yyyy-MM-dd" }
                         }
                     }),
                     new sap.m.Text({
-                        text: "{PlantText}"
+                        text: "{worklistView>PlantText}"
                     }),
                     new sap.m.Text({
-                        text: "{RegionText}"
+                        text: "{worklistView>RegionText}"
                     }),
-                    new sap.m.Input({
-                        value: {
-                            path: "Description",
-                            mode: 'TwoWay'
-                        }
+                    new sap.m.Text({
+                        text:  "{worklistView>Description}"   
                     }),
                     new sap.m.Text({
 						text: {
-                            path: "Created",
+                            path: "worklistView>Created",
                             type: "sap.ui.model.type.Date",
                             formatOptions: { pattern: "yyyy-MM-dd" }
                         }
                     }),
                     new sap.m.Switch({
-                        switchType: "AcceptReject",
-                        state: "{= ${Version} === 'D'}",
+                        state: "{= ${worklistView>Version} === 'D'}",
                         change: this.ChangeVersion.bind(this)
                     }),
                 ]
             });
             return oTemplate;
         },
-        onSaveChanges: function () {
-            const oModel = this.getModel();
-            const oPendingChanges = oModel.getPendingChanges();
-
-            if (oPendingChanges && Object.keys(oPendingChanges).length > 0) {
-                this.getView().setBusy(true);
-                oModel.submitChanges({
-                    success: () => {
-                        this.getView().setBusy(false);
-						this.getModel().refresh(true);
-                        MessageToast.show("Changes saved successfully.");
-						
-                    },
-                    error: () => {
-                        this.getView().setBusy(false);
-                        MessageToast.show("Error saving changes.");
-                    }
-                });
-            } else {
-                MessageToast.show("No changes to save.");
-            }
-        },
-		_getTableFilters: function () {
-			const oWorkListModel = this.getModel('worklistView');
-			const sSelectedKey = oWorkListModel.getProperty('/sITBKey');
-			return sSelectedKey === 'All' ? [] : [new Filter('Version', 'EQ', 'D')];
-		},
 		_getTotalItems: function () {
 			// CountMode - Inline установил в моделе в manifest.json
 			const oBinding = this.getView().byId("table").getBinding("items");
@@ -149,6 +147,48 @@ sap.ui.define([
 			this.getModel('worklistView').setProperty('/sITBKey', oSelctedKey);
 			this._bindTable();
 		},
+		onApplyFilter: function() {
+            var oCombinedFilter = new Filter({
+                filters: [new Filter("Version", FilterOperator.EQ, "D"), new Filter("Description", FilterOperator.EQ, "333")],
+                and: true
+            });
+
+            var oTable = this.byId("table");
+            var oBinding = oTable.getBinding("items");
+            oBinding.filter([oCombinedFilter]);
+
+            this._getTotalItems();
+        },
+		onApplyPartialFilter: function() {
+            var oCombinedFilter = new Filter({
+                filters: [new Filter("Version", FilterOperator.EQ, "D"), new Filter("Description", FilterOperator.EQ, "333")],
+                and: false
+            });
+
+            var oTable = this.byId("table");
+            var oBinding = oTable.getBinding("items");
+            oBinding.filter([oCombinedFilter]);
+
+            this._getTotalItems();
+        },
+        onClear: function() {
+            var oTable = this.byId("table");
+            var oBinding = oTable.getBinding("items");
+            oBinding.filter([]);
+			oBinding.sort(null)
+            this._getTotalItems();
+        },
+
+        onApplySort: function() {
+            var oSorter1 = new Sorter("RegionText", false);
+            var oSorter2 = new Sorter("DocumentNumber", true);
+
+            var oTable = this.byId("table");
+            var oBinding = oTable.getBinding("items");
+            oBinding.sort([oSorter1, oSorter2]);
+
+            this._getTotalItems();
+        },
 
 	});
 });
